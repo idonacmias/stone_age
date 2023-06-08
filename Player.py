367e88d13e8e59d917ad6febcb48ae913dd374e8
idonacmias <idonacmi@gmail.com>
2023-06-08 13:08:38 +0300

    add Player model with score and tribe members, add assigned_phase loop to manage players place tribe members on the bord and switching players turns, psedo cod choose_task

---
+++ Player.py
@@ -0,0 +1,8 @@
+
+
+class Player:
+
+    def __init__(self):
+        self.points = 0
+        self.tribe_members = 5
+
--- app.py
+++ app.py
@@ -1,18 +1,39 @@
-
+from Player import Player
 
 
 def main():
-    players = [0,1,2]
+    players = [Player() for _ in range(3)]
     cards = [i for i in range(10)]
 
     while len(cards) > 3:
-        assigned_phase()
+        assigned_phase(players)
         collecting_phase()
         change_first_palce_player(players)
         print(players)
 
-def assigned_phase():
-    pass
+def assigned_phase(players):
+    corent_player = 0
+    len_players = len(players)
+    while tribe_members_lefts_to_place(players):
+        assigned_tribe_member_to_task(players[corent_player])
+        switch_player(corent_player, len_players)
+
+def tribe_members_lefts_to_place(players):
+    return sum([player.tribe_members for player in players]) > 0
+
+def assigned_tribe_member_to_task(corent_player):
+    tribe_members_in_task = choose_task()
+    corent_player.tribe_members -= tribe_members_in_task
+
+def choose_task():
+    pass    
+
+def switch_player(corent_player, len_players):
+    if corent_player == len_players - 1:
+        corent_player = 0
+
+    else:
+        corent_player += 1
 
 def collecting_phase():
     pass
